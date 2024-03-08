b2f4163f838a117bfc4f03220d700da4cdd7d980
panwarkunal <monupanwar5655@gmail.com>
2024-03-08 16:08:47 +0530

    landingpage

---
+++ landingpage/src/App.jsx
@@ -0,0 +1,48 @@
+// import React from "react";
+// import Card from './card'; // Assuming card.jsx or card.js is in the same directory as App.jsx
+
+// const App = () => {
+//   return (
+//     <>
+//       <Card />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import Hero from './components/hero';
+import Nav from './components/Navigation';
+const App=()=> {
+  return (
+
+
+    <>
+    
+    <div className='overflow-hidden'>
+   
+   <Nav />
+   
+    <Hero />
+     
+    </div>  
+
+    
+    
+    
+    
+    </>   
+
+  
+
+   
+   
+   
+
+
+  );
+}
+
+export default App;
